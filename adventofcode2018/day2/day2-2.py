5d3d756bbab8a1a7be68343653c26d14a27c37fd
Ken Kuo <kkuo@slb.com>
2018-12-04 10:02:39 -0600

    day2 complete

--- adventofcode2018/day2/day2-2.py
+++ adventofcode2018/day2/day2-2.py
@@ -1,26 +1,19 @@
-from collections import defaultdict
-with open('input.txt', 'r') as f:
-  twice = 0
-  thrice = 0
-  for line in f:
-    for line_inner in f:
-      for idx, c in enumerate(line):
-
-    linedict = defaultdict(int)
-    for c in line:
-      linedict[c] += 1
-    twiceline = False
-    thriceline = False
-    for k,v in linedict.items():
-      if v == 2:
-        twiceline = True
-      if v == 3:
-        thriceline = True
-    if twiceline == True:
-      twice += 1
-    if thriceline == True:
-      thrice += 1
-  print(twice)
-  print(thrice)
-  print(twice*thrice)
+def compare_words(a, b):
+  same_letters = []
+  for i,c in enumerate(a):
+    if c == b[i]:
+      same_letters.append(c)
+  if len(same_letters) == len(a)-1:
+    return same_letters
+  else:
+    return False
     
+
+with open('input.txt', 'r') as f:
+  data = f.read()
+  for line in data.split():
+    for line_inner in data.split():
+      same = compare_words(line.strip(), line_inner.strip())
+      if same:
+        print(same)
+        exit()
